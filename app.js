a2a368a90af1cf9b9b02024896adeffca61ff8e5
nixon-tg <elotoja56@gmail.com>
2026-08-02 10:29:20 +0200

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const ids=['home','catalog','dead','contact'];
-function show(id){ids.forEach(x=>document.getElementById(x).classList.add('hidden'));document.getElementById(id).classList.remove('hidden');}
-catalogBtn.onclick=()=>show('catalog');
-contactBtn.onclick=()=>show('contact');
-catalogBack.onclick=()=>show('home');
-deadBtn.onclick=()=>show('dead');
-deadBack.onclick=()=>show('catalog');
-contactBack.onclick=()=>show('home');
-writeBtn.onclick=()=>{const u='https://t.me/elotonieja';if(window.Telegram&&Telegram.WebApp){Telegram.WebApp.openTelegramLink(u);}else{window.open(u,'_blank');}};
